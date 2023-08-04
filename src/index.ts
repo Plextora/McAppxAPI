a3f5d454b197aca4b18a45ce87c2750856b5ec1b
Plextora <71889427+Plextora@users.noreply.github.com>
2023-08-04 14:41:13 -0400

    Some things

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,40 @@
 import express = require("express");
+import("node-fetch");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+function removeLinesWithKeywords(inputString: string) {
+  const keywords = [
+    "BlockMap",
+    "x86",
+    "arm",
+    "Microsoft.VCLibs",
+    "Microsoft.Services.Store.Engagement",
+  ];
+  const lines = inputString.split("\n");
+  const filteredLines = lines.filter((line) => {
+    return !keywords.some((keyword) => line.includes(keyword));
+  });
+  return filteredLines.join("\n");
+}
+
+function removeNonMatchingSubstring(inputString: string) {
+  const keywords = [
+    "tlu.dl.delivery.mp.microsoft.com/filestreamingservice/files",
+    "undefined",
+    "Microsoft.MinecraftUWP",
+  ];
+
+  const filteredString = inputString
+    .split("\n")
+    .filter((line) => {
+      return keywords.some((keyword) => line.includes(keyword));
+    })
+    .join("\n");
+
+  return filteredString;
+}
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
