f4c5dbe8d0a1644f2259dc75dc34315c3024575f
Plextora <71889427+Plextora@users.noreply.github.com>
2023-08-04 20:03:05 -0400

    Added rate limiting

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 import express = require("express");
 import("node-fetch");
+const rateLimit = require("express-rate-limit");
 // imports are like that cause intellisense
 
 const app = express();
+
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 5 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+app.use(limiter);
 
 const PORT = process.env.PORT || 3000;
 
